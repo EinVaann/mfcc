d498dced0c935f6e30d2423218310925b5fca293
EinVaann <44147557+EinVaann@users.noreply.github.com>
2022-01-21 22:58:14 +0700

    rename 2 file

--- 2/getFileMFCC.m
+++ 2/getFileMFCC.m
@@ -1,13 +1,10 @@
-function  huanluyen = getFileHL()
+function  huanluyen = getFileMFCC(NumCoeffs,NumCluster)
     path='NguyenAmHuanLuyen';
     
     people= {'01MDA' '02FVA' '03MAB' '04MHB' '05MVB' '06FTB' '07FTC' '08MLD' '09MPD' '10MSD' '11MVD' '12FTD' '14FHH' '15MMH' '16FTH' '17MTH' '18MNK' '19MXK' '20MVK' '21MTL' '22MHL'};
     vowel = {'a' 'e' 'i' 'o' 'u'};
     color={'blue' 'red' 'yellow' 'black' 'green'};
       
-    NumCoeffs = 13;
-    NumCluster = 1;
-
     huanluyen = cell(5,1);
     for j = 1: length(vowel)
         data = zeros(length(people),NumCoeffs);
--- 2/main.m
+++ 2/main.m
@@ -3,8 +3,10 @@ clc
 
 % s1 = 0;
 % for l = 1:10
-huanluyen = getFileHL();
-confusionMatrix = checking(huanluyen)
+NumCoeffs = 13;
+NumCluster = 3;
+huanluyen = getFileMFCC(NumCoeffs,NumCluster);
+confusionMatrix = checkingMFCC(huanluyen,NumCoeffs)
 correctRate = getCorrectRate(confusionMatrix)
 %     s1 = s1+percentage;
 % end
